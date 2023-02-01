962dc243d439c5dc86531ec89b31d16b63d69cff
Mohammad Rahimi <moyi.pary@gmail.com>
2023-02-01 16:19:48 +0330

    just the things before working on...

--- src/domain/CSE.java
+++ src/domain/CSE.java
@@ -1,40 +1,40 @@
 package domain;
+
 import java.util.Date;
 
 public class CSE {
-	private Course course;
-	private int section;
-	private Date examDate;
+    private Course course;
+    private int section = 1;
+    private Date examDate;
 
-	public CSE(Course course) {
-		this.course = course;
-		this.section = 1;
-		this.examDate = null;
-	}
+    public CSE(Course course) {
+        this.course = course;
+    }
 
-	public CSE(Course course, Date examDate) {
-		this.course = course;
-		this.section = 1;
-		this.examDate = examDate;
-	}
+    public CSE(Course course, Date examDate) {
+        this.course = course;
+        this.examDate = examDate;
+    }
 
-	public CSE(Course course, Date examDate, int section) {
-		this.course = course;
-		this.section = section;
-		this.examDate = examDate;
-	}
-	
-	public Course getCourse() {
-		return course;
-	}
-	
-	public String toString() {
-		return course.getName() + " - " + section;
-	}
-	
-	public Date getExamTime() {
-		return examDate;
-	}
+    public CSE(Course course, Date examDate, int section) {
+        this.course = course;
+        this.section = section;
+        this.examDate = examDate;
+    }
 
-	public int getSection() { return section; }
+    public Course getCourse() {
+        return course;
+    }
+
+    public Date getExamTime() {
+        return examDate;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public String toString() {
+        return course.getName() + " - " + section;
+    }
 }
---
+++ src/domain/Main.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Course course = new Course("CSC101", "Introduction to Computer Science", 3);
+        System.out.println(course);
+    }
+}
